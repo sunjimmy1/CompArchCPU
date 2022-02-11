d9d767d15ef05b0c0d2e6be31256876eeb7c6278
James Sunbury <sunjimmy@comcast.net>
2022-02-11 18:53:02 -0500

    Further Improvements to TB
    The test bench now test well over the 20 required registers

--- testbench.v
+++ testbench.v
@@ -9,7 +9,8 @@ module testbench(output reg [4:0] rs1,
 					 output [31:0] out2,
 					 output reg rw);
 
-
+	reg [5:0] counter;
+					 
 	RegFile testing (rs1, rs2, rd, din, out1, out2, clk, reset, enable, rw);
 		
 	initial begin
@@ -24,11 +25,28 @@ module testbench(output reg [4:0] rs1,
 			rd <= 5'd17;
 			#3
 			rw = 0;
-			#3
-			rd <= 5'd1;
 			rs1 <= 5'd17;
 			rs2 <= 5'd5;
+			for(counter = 1; counter < 5'd31; counter = counter + 2'd2)begin
+				#3
+				din <= 1 << counter;
+				rd <= counter;
+				rw <= 1;
+				#3
+				rw <= 0;
+				#3
+				din <= 32'd1073741824 >> counter;
+				rd <= counter + 1;
+				rw <= 1;
+				#3
+				rw = 0;
+				rs1 <= counter;
+				rs2 <= counter + 1;
+			end
 			#3
+			reset <= 1;
+			#3
+			reset <= 0;
 			$stop;
 		end
 		
