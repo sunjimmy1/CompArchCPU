c0f0c6e4862ff8c27342ec521d8cb3c66df7b9f3
James Sunbury <sunjimmy@comcast.net>
2022-02-22 10:55:26 -0500

    ALU Probaly works
    Most likely

--- testbench.v
+++ testbench.v
@@ -9,10 +9,7 @@ module testbench(output reg clk,
 	ALU testing (clk, in1, in2, op, result, status);
 		
 	initial begin
-			$display("%b", 0 ^ 0 ^ 0); //0
-			$display("%b", 0 ^ 0 ^ 1); //1
-			$display("%b", 1 ^ 0 ^ 1); //0
-			$display("%b", 1 ^ 1 ^ 1); //1
+			$display("I am smort");
 			clk <=0;
 			#5
 			in1 <= 32'b0101;
@@ -31,9 +28,31 @@ module testbench(output reg clk,
 			in2 <= 32'b0111_0111_0000_1111_0000_1111_0000_0101;
 			op <= 4'b0100;
 			#5
-			in1 <= 32'b1111_1111_0000_1111_0000_1111_0000_0101;
-			in2 <= 32'b1111_1111_0000_1111_0000_1111_0000_0101;
+			in1 <= 32'd4;
+			in2 <= 32'd5;
 			op <= 4'b0100;
+			#10
+			op <= 4'd1;
+			#10
+			op <= 4'd2;
+			#10
+			op <= 4'd3;
+			#10
+			op <= 4'd4;
+			#10
+			op <= 4'd5;
+			#10
+			op <= 4'd6;
+			#10
+			op <= 4'd7;
+			#10
+			op <= 4'd8;
+			#10
+			op <= 4'd9;
+			#10
+			op <= 4'd10;
+			#10
+			op <= 4'd11;
 			#10
 			in1 <= 32'b0;
 			in2 <= 32'b0;
