6672379040799df47b10ad1f0de889e33b1caf30
Aysha Sohail <56938770+ASohail22@users.noreply.github.com>
2022-03-01 11:01:57 -0500

    Add files via upload
    all my files for testbench and rom

--- testbench.v
+++ testbench.v
@@ -1,3 +1,40 @@
+/*module testbench(output [31:0] out1, 
+					output [31:0] out2, 
+					output reg [31:0] in1, 
+					output reg [4:0] rs1, 
+					output reg [4:0] rs2, 
+					output reg [4:0] rd, 
+					output reg clk, 
+					output reg EN, 
+					output reg RW, 
+					output reg reset);
+
+RegFile testing(out1, out2, rs1, rs2, rd, clk, EN, RW, in1, reset);
+
+initial begin
+	EN <= 1;
+	RW <= 0;
+	clk <=0;
+	reset <= 1;
+	#15
+	reset <= 0;
+	in1 <= 32'd37;
+	rd <= 5'd17;
+	#5
+	RW <= 1;
+	#5
+	rs1 <= 5'd17;
+	rs2 <= 5'd3;
+	#25
+	$stop;
+end
+
+always begin
+	#1
+	clk <= ~clk;	
+end
+endmodule
+
 module testbench(output reg clk,
 				output reg [31:0] in1,
 				output reg [31:0] in2, 
@@ -6,7 +43,7 @@ module testbench(output reg clk,
 				output wire [4:0] status);
 
 				
-	ALU testing (clk, in1, in2, op, result, status);
+	alu testing (clk, in1, in2, op, result, status);
 		
 	initial begin
 			$display("I am smort");
@@ -63,4 +100,28 @@ module testbench(output reg clk,
 		#1
 		clk <= ~clk;
 	end
+endmodule*/
+
+module testbench(output reg [6:0] address, 
+						output reg en, 
+						output [31:0] out, 
+						output reg clk);
+
+rom testing(address, en, out, clk);
+integer i;
+initial begin
+	clk <= 0;
+	en <= 1;
+	#2
+	for(i = 7'b0; i < 7'b1111111; i = i + 1) begin
+		#2
+		address <= i;
+	end
+	$stop;	
+end
+
+always begin
+	#1
+	clk <= ~clk;
+end
 endmodule
