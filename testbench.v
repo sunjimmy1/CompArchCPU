431626e2861cbc637086cf12f677e9744d6fe9db
James Sunbury <sunjimmy@comcast.net>
2022-04-12 10:57:02 -0400

    Changes to ROM and LW/SW Work
    ROM is now byte addressable and LW/SW work

--- testbench.v
+++ testbench.v
@@ -13,42 +13,42 @@ module testbench();
 					WB;
 	wire [3:0] ALUOP;
 	wire [11:0] imm;
-Datapath d(clk,
-				reset,
-				WB,
-				PCSelect, 
-				RegWEn, 
-				ImmSel, 
-				BrUn, 
-				BEQ, 
-				BLT, 
-				BSel, 
-				ASel, 
-				ALUOP, 
-				WBSel, 
-				MemRW, 
-				longImm, 
-				IWord, 
-				data1Val, 
-				data2Val, 
-				result,
-				alu1, 
-				alu2, 
-				PCVal,
-				imm);
+	
+	Datapath d(clk,
+					reset,
+					WB,
+					PCSelect, 
+					RegWEn, 
+					ImmSel, 
+					BrUn, 
+					BEQ, 
+					BLT, 
+					BSel, 
+					ASel, 
+					ALUOP, 
+					WBSel, 
+					MemRW, 
+					longImm, 
+					IWord, 
+					data1Val, 
+					data2Val, 
+					result,
+					alu1, 
+					alu2, 
+					PCVal,
+					imm);
 
-initial begin
-	$display(4'b0111+4'b1100);
-	clk <= 1'b0;
-	reset <= 1;
-	#1
-	reset <= 0;
-	#64
-	$stop;	
-end
+	initial begin
+		clk <= 1'b0;
+		reset <= 1;
+		#1
+		reset <= 0;
+		#64
+		$stop;	
+	end
 
-always begin
-	#1
-	clk <= ~clk;
-end
+	always begin
+		#1
+		clk <= ~clk;
+	end
 endmodule
